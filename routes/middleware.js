aa149cd31df7c8e55b6b6814b17bdd56cd3d8c43
Ian <Ian5015@live.com>
2014-10-16 19:36:18 -0400

    changed port to 80

--- routes/middleware.js
+++ routes/middleware.js
@@ -29,7 +29,8 @@ exports.initLocals = function(req, res, next) {
 		{ label: 'Home',		key: 'home',		href: '/' },
 		{ label: 'Blog',		key: 'blog',		href: '/blog' },
 		{ label: 'Gallery',		key: 'gallery',		href: '/gallery' },
-		{ label: 'Contact',		key: 'contact',		href: '/contact' }
+		{ label: 'Contact',		key: 'contact',		href: '/contact' },
+		{ label: 'Forums',		key: 'forums',		href: '/forums' }
 	];
 	
 	locals.user = req.user;
